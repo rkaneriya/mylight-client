f6e3898d490f7ff079c2b319234376d230b9b7e7
rkaneriya <rkaneriya@gmail.com>
2015-10-21 11:28:24 -0400

    added validation to new user account input fields, including username/email

--- app/js/components/CreateAccount.js
+++ app/js/components/CreateAccount.js
@@ -5,10 +5,12 @@ var Navigation = Router.Navigation;
 
 var MAIN_URL = require('../../config/config').MAIN_URL; 
 var UserActions = require('../actions/UserActions');
+var NewUserActions = require('../actions/NewUserActions'); 
 var NewUserStore = require('../stores/NewUserStore');  
 var auth = require('../utils/auth'); 
 
 var Col = require('react-bootstrap').Col; 
+var Alert = require('react-bootstrap').Alert; 
 
 var CreateAccount = React.createClass({
     mixins: [Navigation, Reflux.connect(NewUserStore)],
@@ -32,6 +34,10 @@ var CreateAccount = React.createClass({
             this.context.router);
     }, 
 
+    handleInfoChange: function(prop) {
+        NewUserActions.updatePersonalInfo(prop, document.getElementById(prop).value);
+    },
+
     render: function() {
         return (    
             <div className="container">
@@ -43,42 +49,41 @@ var CreateAccount = React.createClass({
                     <span style={{ color: 'red' }}>* Required</span>
                     
                     <br/><br/>
-
                     <div>
                         <span className="create-account-label">First Name: <span style={{ color: 'red' }}>*</span></span>
-                        { (this.state.isValid.first_name) ? 
-                            <input type="text" id="first_name" className="form-control" placeholder="" ref="first_name" required autofocus/> :
-                            <input type="text" id="first_name" className="form-control create-account-error" placeholder="" ref="first_name" required autofocus/>
+                        <input type="text" id="first_name" className="form-control" placeholder="" ref="first_name" value={ this.state.personal_info.first_name } onChange={ this.handleInfoChange.bind(this, 'first_name') } required autofocus/> 
+                        { (!this.state.isValid.first_name) ? 
+                            <Alert bsStyle='warning'>You must provide a first name.</Alert> : ""
                         }
 
                         <span className="create-account-label">Last Name: <span style={{ color: 'red' }}>*</span></span>
-                        { (this.state.isValid.last_name) ? 
-                            <input type="text" id="last_name" className="form-control" placeholder="" ref="last_name" required autofocus/> :
-                            <input type="text" id="last_name" className="form-control create-account-error" placeholder="" ref="last_name" required autofocus/>
-                        }
-
-                        <span className="create-account-label">Email:</span>
-                        { (this.state.isValid.email) ? 
-                            <input type="text" id="email" className="form-control" placeholder="" ref="email" required autofocus/> : 
-                            <input type="text" id="email" className="form-control create-account-error" placeholder="" ref="email" required autofocus/>
+                        <input type="text" id="last_name" className="form-control" placeholder="" ref="last_name" value={ this.state.personal_info.last_name } onChange={ this.handleInfoChange.bind(this, 'last_name') } required/> 
+                        { (!this.state.isValid.last_name) ? 
+                            <Alert bsStyle='warning'>You must provide a last name.</Alert> : ""
                         }
 
                         <span className="create-account-label">Choose your username: <span style={{ color: 'red' }}>*</span></span>                        
-                        { (this.state.isValid.username) ? 
-                            <input type="text" id="username" className="form-control" placeholder="" ref="username" required autofocus/> : 
-                            <input type="text" id="username" className="form-control create-account-error" placeholder="" ref="username" required autofocus/>
+                        <input type="text" id="username" className="form-control" placeholder="" ref="username" value={ this.state.personal_info.username } onChange={ this.handleInfoChange.bind(this, 'username') } required/> 
+                        { (!this.state.isValid.username) ? 
+                            <Alert bsStyle='warning'>You must provide a unique username.</Alert> : ""
                         }
                        
                         <span className="create-account-label">Create a new password: <span style={{ color: 'red' }}>*</span></span>
-                        { (this.state.isValid.password) ? 
-                            <input type="password" id="password" className="form-control" placeholder="" ref="password" required/> :
-                            <input type="password" id="password" className="form-control create-account-error" placeholder="" ref="password" required/>
+                        <input type="password" id="password" className="form-control" placeholder="" ref="password" value={ this.state.personal_info.password } onChange={ this.handleInfoChange.bind(this, 'password') } required/> 
+                        { (!this.state.isValid.password) ? 
+                            <Alert bsStyle='warning'>Password must be longer than 5 characters.</Alert> : ""
                         }
 
                         <span className="create-account-label">Confirm your password: <span style={{ color: 'red' }}>*</span></span>
-                        { (this.state.isValid.confirmPassword) ? 
-                            <input type="password" id="confirmPassword" className="form-control" placeholder="" ref="confirmPassword" required/> :
-                            <input type="password" id="confirmPassword" className="form-control create-account-error" placeholder="" ref="confirmPassword" required/>
+                        <input type="password" id="confirmPassword" className="form-control" placeholder="" ref="confirmPassword" value={ this.state.personal_info.confirmPassword } onChange={ this.handleInfoChange.bind(this, 'confirmPassword') } required/> 
+                        { (!this.state.isValid.confirmPassword) ? 
+                            <Alert bsStyle='warning'>Passwords must match.</Alert> : ""
+                        }
+
+                        <span className="create-account-label">Email:</span><span className='create-account-sublabel'> (for password recovery only)</span>
+                        <input type="text" id="email" className="form-control" placeholder="" ref="email" value={ this.state.personal_info.email } onChange={ this.handleInfoChange.bind(this, 'email') }/> 
+                        { (!this.state.isValid.email) ? 
+                            <Alert bsStyle='warning'>This email address has already been used to create an account.</Alert> : ""
                         }
 
                         <button className="btn btn-lg btn-primary btn-block" onClick={ this.onCreate }>Create Account</button>
--- app/js/stores/NewUserStore.js
+++ app/js/stores/NewUserStore.js
@@ -3,6 +3,7 @@ var request = require('request');
 var _ = require('underscore'); 
 
 var UserActions = require('../actions/UserActions');
+var NewUserActions = require('../actions/NewUserActions'); 
 
 var ApiClient = require('../utils/api-client'); 
 var MAIN_URL = require('../../config/config').MAIN_URL; 
@@ -16,29 +17,33 @@ var _isValid = {
     confirmPassword: true 
 }; 
 
+var _personal_info = { 
+    first_name: '', 
+    last_name: '',
+    email: '',
+    username: '',
+    password: '',
+    confirmPassword: ''
+}
+
 var NewUserStore = Reflux.createStore({
-    listenables: [UserActions], 
+    listenables: [UserActions, NewUserActions], 
 
     getInitialState: function() {
         return { 
-            isValid: _isValid
+            isValid: _isValid,
+            personal_info: _personal_info
         };
     },
 
     onCreateNewUser: function(first_name, last_name, username, email, password, confirmPassword, router) { 
         var self = this;
 
-        _isValid.first_name = (first_name.trim() !== ''); 
-
-        _isValid.last_name = (last_name.trim() !== ''); 
-
-        _isValid.username = (username.trim() !== ''); 
-
-        // no email validation yet 
-
-        _isValid.password = (password.length > 5) && (password == confirmPassword); 
-
-        _isValid.confirmPassword = (confirmPassword.length > 5) && (password == confirmPassword);
+        _isValid.first_name = _isValid.first_name && (first_name.trim() !== ''); 
+        _isValid.last_name = _isValid.last_name && (last_name.trim() !== ''); 
+        _isValid.username = _isValid.username && (username.trim() !== ''); 
+        _isValid.password = _isValid.password && (password.trim() !== ''); 
+        _isValid.confirmPassword = _isValid.confirmPassword && (confirmPassword.trim() !== ''); 
 
         if (!_.contains(_.values(_isValid), false)) { 
             ApiClient.createNewUser(first_name, last_name, email, username, password, function(res) { 
@@ -46,6 +51,7 @@ var NewUserStore = Reflux.createStore({
                     localStorage.setItem('jwt', res.jwt); 
                     router.transitionTo('/mylight'); 
                 } else { 
+                    console.log(res); 
                     _isValid.username = res.username; 
                     _isValid.email = res.email; 
                     self.trigger({ isValid: _isValid }); 
@@ -58,6 +64,36 @@ var NewUserStore = Reflux.createStore({
             self.trigger({ isValid: _isValid }); 
         }
     }, 
+
+    onUpdatePersonalInfo: function(prop, newValue) { 
+        var self = this; 
+
+        switch(prop) { 
+            case 'first_name':
+                _isValid.first_name = (newValue.trim() !== ''); 
+                break;
+            case 'last_name':
+                _isValid.last_name = (newValue.trim() !== ''); 
+                break;
+            case 'username': 
+                _isValid.username = (newValue.trim() !== ''); 
+                break;
+            case 'password':
+                _isValid.password = (newValue.length > 5); 
+                break;
+            case 'confirmPassword':
+                _isValid.confirmPassword = (newValue == _personal_info.password);
+                break;
+            case 'email':
+                _isValid.email = true; // change this later 
+                break;
+            default: 
+                break;
+        }
+
+        _personal_info[prop] = newValue; 
+        this.trigger({ personal_info: _personal_info, isValid: _isValid }); 
+    }
 });
 
 
